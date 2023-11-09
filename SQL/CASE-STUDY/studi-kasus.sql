1049105186206b6e971fa88f3dcad92e28c70788
Galang Arsandy <galangarsandy@gmail.com>
2023-11-10 03:39:56 +0700

    Merapikan Kode

--- SQL/CASE-STUDY/studi-kasus.sql
+++ SQL/CASE-STUDY/studi-kasus.sql
@@ -1,3 +1,5 @@
+
+-- NO 1
 -- 1 pelanggan membeli 3 barang yang berbeda.
 
 -- pelanggan dengan id_pelanggan = 1 melakukan pemesanan dengan id_pembayaran = 3, pada tgl = 10 Nov 2023. id pesanannya = 1111
@@ -23,3 +25,44 @@ SELECT pesanan.id,
     JOIN produk ON item_pesanan.id_produk = produk.id
     WHERE pesanan.id = 1111
     GROUP BY pesanan.id;
+
+-- ============================================================================================
+-- ============================================================================================
+
+-- NO 2
+-- Melihat 3 produk yang paling sering dibeli oleh pelanggan.
+SELECT id_produk, nama_produk, SUM(kuantitas) AS total_terjual
+    FROM item_pesanan
+    JOIN produk ON item_pesanan.id_produk = produk.id
+    GROUP BY id_produk
+    ORDER BY SUM(kuantitas) DESC
+    LIMIT 3;
+
+-- ============================================================================================
+-- ============================================================================================
+
+-- NO 3
+-- Melihat Kategori barang yang paling banyak barangnya.
+SELECT kategori.nama_kategori, COUNT(produk.id_kategori) AS total_produk
+    FROM produk
+    JOIN kategori ON produk.id_kategori = kategori.id
+    GROUP BY kategori.id
+    ORDER BY COUNT(produk.id_kategori) DESC;
+
+-- ============================================================================================
+-- ============================================================================================
+
+-- NO 4
+-- Nominal rata-rata transaksi yang dilakukan oleh pelanggan dalam 1 bulan terakhir.
+SELECT nama_pelanggan, CONCAT('Rp', FORMAT(AVG(total_harga),0)) AS rata_transaksi_1_bulan_terakhir
+    FROM (
+        SELECT SUM(produk.harga * item_pesanan.kuantitas) AS total_harga,
+        pelanggan.nama AS nama_pelanggan
+        FROM pesanan
+        JOIN item_pesanan ON pesanan.id = item_pesanan.id_pesanan
+        JOIN pelanggan ON pesanan.id_pelanggan = pelanggan.id
+        JOIN produk ON item_pesanan.id_produk = produk.id
+        WHERE pesanan.tanggal_pesan >= DATE_SUB(NOW(), INTERVAL 1 MONTH)
+        AND  pesanan.id_pelanggan = 8 -- ditampilkan berdasarkan id pelanggan
+        GROUP BY pesanan.id
+    )AS rata_transaksi_1_bulan_terakhir;
--- SQL/DML/mengubah-data-tabel.sql
+++ SQL/DML/mengubah-data-tabel.sql
@@ -1,11 +1,11 @@
 UPDATE pelanggan
-SET nama = 'Galang Arsandy', email = 'glangarsandy@gmail.com', hp = 6285157994646
-WHERE id = 1;
+    SET nama = 'Galang Arsandy', email = 'glangarsandy@gmail.com', hp = 6285157994646
+    WHERE id = 1;
 
 UPDATE alamat
-SET desa = 'Desa Perubahan', kecamatan = 'Kecamatan Baru-Baru'
-WHERE id = 1;
+    SET desa = 'Desa Perubahan', kecamatan = 'Kecamatan Baru-Baru'
+    WHERE id = 1;
 
 UPDATE produk
-SET harga = 7000000
-WHERE id = 11;
+    SET harga = 7000000
+    WHERE id = 11;
